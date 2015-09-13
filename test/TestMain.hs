2e08f0a382b04c30ebcaa1249b95ce5af47f8d44
Alexander Batischev <eual.jp@gmail.com>
2015-09-13 03:04:28 +0300

    Test scenarios where he buys

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -13,12 +13,15 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests = testGroup "Unit tests"
-  [ foodForMyself
-  , foodForHim
-  , foodForBoth
+  [ iBoughtFoodForMyself
+  , iBoughtFoodForHim
+  , iBoughtFoodForBoth
+  , heBoughtFoodForMe
+  , heBoughtFoodForHimself
+  , heBoughtFoodForBoth
   ]
 
-foodForMyself =
+iBoughtFoodForMyself =
   let actions = [ (10, ForMe, Food) ]
       expectedState = defaultState {
         food   = 10
@@ -27,7 +30,7 @@ foodForMyself =
   in testCase "I bought food for myself" $
        runActions Me actions @?= expectedState
 
-foodForHim =
+iBoughtFoodForHim =
       -- we won't be updating any of the "expenses" account, so no need to ask
       -- for category. Thus `undefined`
   let actions = [ (10, ForHim, undefined) ]
@@ -38,7 +41,7 @@ foodForHim =
   in testCase "I bought food for him" $
        runActions Me actions @?= expectedState
 
-foodForBoth =
+iBoughtFoodForBoth =
   let actions = [ (10, ForBoth, Food) ]
       expectedState = defaultState {
         food   = 5
@@ -47,3 +50,29 @@ foodForBoth =
       }
   in testCase "I bought food for both" $
        runActions Me actions @?= expectedState
+
+heBoughtFoodForMe =
+  let actions = [ (10, ForMe, Food) ]
+      expectedState = defaultState {
+        food = 10
+      , loan = -10
+      }
+  in testCase "He bought food for me" $
+       runActions He actions @?= expectedState
+
+heBoughtFoodForHimself =
+      -- we won't be updating any of the "expenses" account, so no need to ask
+      -- for category. Thus `undefined`
+  let actions = [ (10, ForHim, undefined) ]
+      expectedState = defaultState
+  in testCase "He bought food for himself" $
+       runActions He actions @?= expectedState
+
+heBoughtFoodForBoth =
+  let actions = [ (10, ForBoth, Food) ]
+      expectedState = defaultState {
+        food   = 5
+      , loan   = -5
+      }
+  in testCase "He bought food for both" $
+       runActions He actions @?= expectedState
